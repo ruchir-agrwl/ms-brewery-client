99c56b1202d1de680142307f599dbbba4eedd7d5
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-07-18 00:35:27 +0530

    Skeleton Code Added Closes #1

---
+++ src/main/java/com/learn/brewery/client/MsBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.learn.brewery.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBreweryClientApplication.class, args);
+	}
+
+}
